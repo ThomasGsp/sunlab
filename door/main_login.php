5482cc9c9a35db0d94e296c1b8eaf5eea445635d
Tlams <tlams@tlams.lan>
2017-01-10 20:28:03 +0100

    changement nom page

--- door/main_login.php
+++ door/main_login.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['username'])) {
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>Login</title>
+    <title>Porte Sunlab</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link href="common/css/bootstrap.css" rel="stylesheet" media="screen">
