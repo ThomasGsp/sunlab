cd69ea96f78b7bf56e2ce67b5f7da426e3631e42
Sébastien TOUZÉ <sebtouze@gmx.fr>
2017-06-06 22:19:47 +0200

    [Trombi] Ajouté un peu de présentation pour la page principale. Refat l'indentation.

--- facebook/index.php
+++ facebook/index.php
@@ -15,27 +15,32 @@ require(dirname(__DIR__) . '/common/includes/userinfo.php');
 <!DOCTYPE html>
 <html lang="en">
 <html>
-<head>
-<title>Trombinoscope</title>
-</head>
-<body>
+  <head>
+    <title>Trombinoscope</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="common/css/bootstrap.css" rel="stylesheet" media="screen">
+    <link href="common/css/main.css" rel="stylesheet" media="screen">
+  </head>
+  <body>
+    <div class="container">
+      <a href="/" ><img src="../common/images/logo_home_maroon.png" class="img-responsive" alt="Logo Sunlab"/></a>
+      <p>Liste des utilisateurs: </p>
+      <ul>
+      <?php
 
-<p>Liste des utilisateurs: </p>
-<ul>
-<?php
+        //ldapGet avec un compte d'application
+        $userList = ldapGet();
 
-//ldapGet avec un compte d'application
-$userList = ldapGet();
+        foreach($userList as $user) {
+        ?>
+        <li><?php echo $user['username'] ?></li>
+        <?php } ?>
+      </ul>
+    </div>
+  </body>
+  <footer>
 
-foreach($userList as $user) {
-?>
-<li><?php echo $user['username'] ?></li>
-<?php } ?>
-</ul>
-
-</body>
-<footer>
-
-</footer>
+  </footer>
 
 </html>
